7c24c5e0b3531312a8f87d06a67785f6150c2f2d
MacMillon-VS <macsimson3@gmail.com>
2022-06-02 15:37:07 +0530

    first commit

---
+++ server/src/config.js
@@ -0,0 +1,7 @@
+// Replace your AWS user keys here
+module.exports = {
+    awsCreds: {
+        accessKey: 'AKIARR2LPNZMXB4ZOUHH',
+        secretKey: 'W12+hjiGqeQ3IhGxdyI73jQEcJbr3p2rQtYzVQQ9'
+    }
+}
